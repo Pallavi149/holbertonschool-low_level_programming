08177563fec3c4d163fd623ac4cabf0ff75d58fa
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-02 18:47:34 -0700

    Modified last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,7 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastdigit = n % 10;
-	if (lastdigit < 6)
+	if (lastdigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+	}
+	else if (lastdigit < 6)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 		       n, lastdigit);
