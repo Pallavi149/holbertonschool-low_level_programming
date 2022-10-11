d56e3ef7ea8c67396ec1196d7d33ce3d312ccac7
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-11 02:19:17 -0700

    sting compare

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include"main.h"
+
+/**
+*_strcmp - Compares two string
+*@s1: First string
+*@s2: second string
+*
+*Return: Always 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+	       	s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0')
+	{
+		return -(*s2);
+	}
+	if (*s2 == '\0')
+	{
+		return (*s1);
+	}
+
+	return (*s1 - *s2);
+}
