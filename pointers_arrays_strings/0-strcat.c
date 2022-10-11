7997c20cad61987eec179a1bf1410c4bb851cfd7
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-11 04:42:53 -0700

    string concat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,7 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	
+
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -23,5 +23,5 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
-	return dest;
+	return (dest);
 }
