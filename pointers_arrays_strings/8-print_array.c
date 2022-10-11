4558d170dc437a0576afe03af7e3c140071ed782
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-10 18:05:59 -0700

    changed file

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 
 /**
- *puts_array - prints an array of an integer
+ *print_array - prints an array of an integer
  *@n: n is the number of elements of the array to be printed
  *@a: an array of integers
  *
