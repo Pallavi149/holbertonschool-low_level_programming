b807c999806342b9504c224f82fc181c97ff7b47
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-10 18:01:49 -0700

    printing array numbers using pointer

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ *puts_array - prints an array of an integer
+ *@n: n is the number of elements of the array to be printed
+ *@a: an array of integers
+ *
+ *Return: Always 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i  = 0;
+
+	while (i < n)
+	{
+	printf("%d", *(a + i));
+	if (i < (n - 1))
+	{
+	printf(", ");
+	}
+	i++;
+	}
+	printf("\n");
+}
