81d20e1e4dd715fba2804361ebded85ed28e8bbb
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-12 01:40:42 -0700

    Locating string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<string.h>
+/**
+* *_strchr - Locates a character in a string
+*@s: source string
+*@c: character to find
+*Return: return the string
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
