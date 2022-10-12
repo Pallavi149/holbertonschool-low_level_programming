7771fadc9095f1ed38c780531ff25d4284c0cca5
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-11 23:07:59 -0700

    updated memcpy file

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,13 +10,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-	char *d = (char *)dest;
-	char *s = (char *)src;
-
 
 	for (i = 0; i < n; i++)
 	{
-		d[i] = s[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
