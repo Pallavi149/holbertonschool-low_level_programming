64f7f6f11f18fad9e39f71fbb352c66d854c1cbd
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-13 01:52:41 -0700

    1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,12 +1,15 @@
 #include"main.h"
 /**
-* *leet - encodes a string into 1337
+**leet - encodes a string into 1337
 *@s: string to encode
+*
+*Return: pointer
 */
 
 char *leet(char *s)
 {
 	char upper_letters[] = "AEOTL";
+	char lower_letters[] = "aeotl";
 	char number_letters[] = "43071";
 	int i, j;
 
@@ -17,17 +20,13 @@ char *leet(char *s)
 		while (upper_letters[j] != '\0')
 		{
 			if (s[i] == upper_letters[j] ||
-			     s[i] == (upper_letters[j] + 'a' - 'A'))
-		{
-			s[i] = number_letters[j];
+			     s[i] == lower_letters[j])
+			{
+				s[i] = number_letters[j];
+			}
 			j = j + 1;
 		}
-		else
-		{
-			j = j + 1;
-		}
-	}
-	i = i + 1;
+		i = i + 1;
 	}
 	return (s);
 }
