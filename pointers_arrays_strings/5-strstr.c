5275dfab548ce1ec9310a1056e87f7c82c98ba73
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-16 01:11:49 -0700

    locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include"main.h"
+#include <stddef.h>
+
+/**
+*_strpbrk - locates the substring
+*@haystack: string
+*@needle: list of characters
+*Return: the pointer or NULL
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int j = 0, len = 0;
+
+	while (needle[len] != '\0')
+	{
+		len = len + 1;
+	}
+
+	while (*haystack != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (*(haystack + j)  != needle[j])
+			{
+				break;
+			}
+			j = j + 1;
+		}
+
+		if (j == len)
+		{
+			return (haystack);
+		}
+		haystack = haystack + 1;
+	}
+	return (NULL);
+}
