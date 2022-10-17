b12af3cdda4ef791c8e61fb91031ad0ee35604b7
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-16 21:54:00 -0700

    factorial of a given number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ *factorial - Returns the factorial of a given number
+ * @n: Given number
+ *Return:Always 0.
+ */
+int factorial(int n)
+{
+	int facto;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+	facto = n * factorial(n - 1);
+	return (facto);
+	}
+}
