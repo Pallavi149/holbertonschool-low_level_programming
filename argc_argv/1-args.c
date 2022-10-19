4abce41e8f21612445e63fef0449725d6dac1370
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-18 17:38:37 -0700

    prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define UNUSED
+/**
+ * main - Entry point of a program
+ * Return: Always 0
+ *@argc: count of the argument supplied to program
+ *@argv: an array of pointers to the strings
+ */
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+       	return (0);
+}
