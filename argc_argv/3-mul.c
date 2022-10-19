e3887affe53507dae958c81974768ebd9fc0afe0
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-18 20:05:25 -0700

    Multiplication

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point of a program
+ * Return: Always 0
+ *@argc: count of the argument supplied to program
+ *@argv: an array of pointers to the strings
+ */
+int main(int argc, char **argv)
+{
+	int mul;
+
+	if (argc > 1)
+	{
+		while (argc >= 3)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+			argc--;
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
