078cb658e1f76b65688cc6c0e89f4ab8bfa3ccea
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-24 20:01:09 -0700

    String copy

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter
+ * @str: Char Pointer
+ * Return: Returns a pointer which contains a copy of the string
+ *given as a parameter
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i, strlen;
+
+	strlen = 0;
+	while (str[strlen] != '\0')
+	{
+		strlen = strlen + 1;
+	}
+	ptr = (char *)malloc(strlen * sizeof(*ptr));
+	if (*str == '\0')
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < strlen)
+	{
+		ptr[i] = str[i];
+		i = i + 1;
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	return (ptr);
+}
