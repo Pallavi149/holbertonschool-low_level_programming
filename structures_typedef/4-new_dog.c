f183ba19eb792ba8dafae5689f5ee65e023a769e
pallavi149 <5811@holbertonschoolstudents.com>
2022-10-31 18:51:19 -0700

    updating new_dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,9 +1,7 @@
-#include<stdio.h>
 #include<stdlib.h>
 #include"dog.h"
 /**
  *new_dog - Creates a new dog
- *@d: pointer to dog_t
  *@name: Name of the dog
  *@age: Age of the dog
  *@owner: Owner
@@ -13,15 +11,60 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *cname, *cowner;
+	int i, j, nlen, olen;
 
-	d = malloc(sizeof(*d));
-	if (d == NULL)
+	nlen = 0;
+	i = 0;
+	while(name[i] != '\0')
 	{
-		free(d);
+		nlen = nlen + 1;
+		i = i + 1;
+	}
+
+	cname = (char *)malloc(nlen * sizeof(char));
+	if (cname == NULL)
+	{
 		return (NULL);
 	}
-	d->name = name;
+	i = 0;
+	while (name[i] != '\0')
+	{
+		cname[i] = name[i];
+		i = i + 1;
+	}
+
+	olen = 0;
+	j = 0;
+	while(owner[j] != '\0')
+	{
+		olen =olen + 1;
+		j = j + 1;
+	}
+	cowner = (char *)malloc(olen * sizeof(char));
+	if (cowner == NULL)
+	{
+		free(cname);
+		return (NULL);
+	}
+	j = 0;
+	while (owner[j] != '\0')
+	{
+		cowner[j] = owner[j];
+		j = j + 1;
+	}
+
+	d = malloc(sizeof(*d));
+
+	if (d == NULL)
+	{
+		free(cname);
+		free(cowner);
+		return (NULL);
+	}
+
+	d->name = cname;
 	d->age = age;
-	d->owner = owner;
+	d->owner = cowner;
 	return (d);
 }
