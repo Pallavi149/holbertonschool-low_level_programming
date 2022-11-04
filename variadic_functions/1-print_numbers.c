9be9d975411a81088cd71a0457936a4875c18747
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-03 19:52:23 -0700

    Print numbers using variadic function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -27,7 +27,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		number = va_arg(ap, int);
 		printf("%d", number);
-		if (i < n - 1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
