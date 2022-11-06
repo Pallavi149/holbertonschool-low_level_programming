7724bd831a97e3c9f45157da410476a9b4cdc043
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-06 05:22:43 -0800

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdarg.h>
+
+
+/**
+ * _print_all - prints anything, followed by a new line
+ * @format: the format of variadic arguments
+ * @j: format length
+ * @ap: arg list
+ * Return: Nothing
+ */
+void _print_all(const char * const format, int j, va_list ap)
+{
+	int i, sep;
+	char *string;
+
+	i = 0;
+	while (i < j)
+	{
+		sep = 0;
+		switch (format[i])
+		{
+		case 's':
+			string = va_arg(ap, char*);
+			if (string == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", string);
+			sep = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			sep = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			sep = 1;
+			break;
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			sep = 1;
+			break;
+		}
+		if (sep == 1 && i < j - 1)
+		{
+			printf(", ");
+		}
+		i = i + 1;
+	}
+}
+
+/**
+ * print_all - prints anything, followed by a new line
+ * @format: the format of variadic arguments
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int j;
+
+	j = 0;
+	while (format[j] != '\0')
+	{
+		j = j + 1;
+	}
+	va_start(ap, format);
+
+	_print_all(format, j, ap);
+	printf("\n");
+	va_end(ap);
+}
