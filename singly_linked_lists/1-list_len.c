9aca5413ca35ec32cc9a8882ff5300fc77003c29
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-14 16:44:41 -0800

    Returning the number of elements from the list_t

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "lists.h"
+
+/**
+ *list_len -  Returns the number of elements of a list_t list
+ *@h: Pointer to list_t
+ *Return: The number of elements of linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != 0)
+	{
+		h = h->next;
+		i = i + 1;
+	}
+	return (i);
+}
