9c8c4a4086fdfd1de1e6e9c20829f303f300d0e2
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-15 16:35:07 -0800

    Adding a node at the beginning of a list

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -3,7 +3,7 @@
 #include"lists.h"
 /**
  **add_nodeint - adds a new node at the beginning of a listint_t list
- *@h: pointer to pointer to  listint_t
+ *@head: pointer to pointer to  listint_t
  *@n: is the constant integer to add to list
  *Return: the address of the new element, or NULL if it failed
  */
