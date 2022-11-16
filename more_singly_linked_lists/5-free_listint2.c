f97417a1dfdca7cef6345e9e34bb05b7e02633cf
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-15 17:28:58 -0800

    freeing list and setting head to NULL

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stddef.h>
+#include "lists.h"
+/**
+ * free_listint2 -  frees a listint_t list
+ *@head: Pointer to pointer to structure listint_t
+ *Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
