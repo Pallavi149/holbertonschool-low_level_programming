8f853c52aad27cc62e0577c84f904986327da956
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-17 19:16:38 -0800

    Fixing betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,8 +2,9 @@
 
 /**
  * *add_dnodeint - adds a new node at the beginning of a dlistint_t list.
- * @h: Pointer to pointer to a dlistint_t list
- * Return: the address of the new element, or NULL if it failed
+ * @head: Pointer to pointer to a dlistint_t list
+ * @n: integer data to add to a node
+ *Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
