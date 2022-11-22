a883dfcd513c749320801de61eea3ebe294be04d
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-21 23:18:32 -0800

    fixing betty

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bin - returns the value of a bit at a given index.
+ * get_bit - returns the value of a bit at a given index.
  * @n: is the unsigned long int
  * @index: is the index, starting from 0 of the bit you want to get
  * Return: the value of the bit at index index or -1 if an error occured
