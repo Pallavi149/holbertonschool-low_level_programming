892d9c2e2110da37afc4a7701e55673f74f5bf68
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-23 16:53:09 -0800

    fixing betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,19 +11,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 	/*hash_node_t *node;*/
-
 	new_table = malloc(sizeof(hash_table_t) * size);
 	if (new_table == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	/*node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-	node->key = key;
-	node->value = value;*/
+	/*
+	 * node = malloc(sizeof(hash_node_t));
+	 *if (node == NULL)
+	 *{
+	 *	return (NULL);
+	 *}
+	 *node->key = key;
+	 *node->value = value;
+	 */
 	return (new_table);
 }
