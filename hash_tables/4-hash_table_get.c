20d1af930ba73c1acefe6205c3e30f15c83d08b9
pallavi149 <5811@holbertonschoolstudents.com>
2022-11-26 15:34:19 -0800

    find value of a key

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,8 +4,7 @@
  * hash_table_get - retrieves a value associated with a key.
  * @ht:  is the hash table you want to look into
  * @key: is the key you are looking for
- * Return: value associated with the element,
- or NULL if key couldn’t be found
+ * Return: value of the element, or NULL if key couldn’t be found
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
