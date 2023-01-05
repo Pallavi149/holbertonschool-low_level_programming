6e1b18b3f22bb7223b318c23098f1aea87b6cda1
pallavi149 <5811@holbertonstudents.com>
2023-01-04 22:18:28 -0800

    Fixed betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,6 +6,7 @@
  *@array: is a pointer to the first element of the array to search in
  *@size: is the number of elements in array
  *@value: is the value to search for
+ *Return: -1 If value is not present in array or if array is NULL
  */
 
 int linear_search(int *array, size_t size, int value)
